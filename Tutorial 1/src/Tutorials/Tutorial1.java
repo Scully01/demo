304802e3b73011daee76a3bdd9f8dc2113b67af1
Scully01 <s3722193@student.rmit.edu.au>
2020-03-10 18:14:02 +1100

    sdsd

---
+++ Tutorial 1/src/Tutorials/Tutorial1.java
@@ -0,0 +1,29 @@
+package Tutorials;
+
+public class Tutorial1 {
+
+    public static void main(String[] args) {
+        int[] items = new int[5];
+        bubblesort(items);
+    }
+
+    public static void bubblesort(int[] sorteditems) {
+        for (int i; i > i+1) {
+            if (int j; j > j-i-1) {
+                if a[j]>a[j+1] {
+                        
+                }
+            }
+        }
+    }
+    procedure bubbleSort(A : list of sortable items)
+        n := length(A)
+            for i from 0 to N-1 do
+                for j from 0 to N-i-1 do
+                    if a[j]>a[j+1] then
+                    swap(a[j], a[j+1])
+                endif
+            endfor
+        endfor
+    end procedure;
+}
